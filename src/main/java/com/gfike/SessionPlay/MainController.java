64b672a8da99bd456e9211199b341ff276da844f
gfike19@gmail.com <Vaughn*573>
2019-05-07 12:05:37 -0500

    got session working

--- src/main/java/com/gfike/SessionPlay/MainController.java
+++ src/main/java/com/gfike/SessionPlay/MainController.java
@@ -8,27 +8,39 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 @Controller
-@RequestMapping("SessionPlay")
+//@RequestMapping("SessionPlay")
 public class MainController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index() {
-        return "SessionPlay/index";
+    public String index(Model model, HttpSession session)
+    {
+        ArrayList<String> words = null;
+        if (session.getAttribute("words") == null) {
+            // where words is just a string
+            //String words = session.getAttribute("words").toString();
+            words = new ArrayList<String>();
+        }
+        else {
+            words = (ArrayList<String>) session.getAttribute("words");
+        }
+        session.setAttribute("words", words);
+        model.addAttribute("words", words);
+        return "index";
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String indexPost (HttpServletRequest request, Model model) {
+    public String indexPost (HttpServletRequest request, Model model, HttpSession session) {
         String sumText = request.getParameter("sumText");
-        model.addAttribute("words", sumText);
+        ArrayList<String> words = (ArrayList<String>) session.getAttribute("words");
+        words.add(sumText);
+        // where words is just a string
+//        session.setAttribute("words", sumText);
+//
+        model.addAttribute("words", words);
         return "redirect:";
     }
 
-//    @RequestMapping(value = "", method = RequestMethod.GET)
-//    public String index(Model model, HttpSession session) {
-//        String words = (String) session.getAttribute("words");
-//        model.addAttribute(words);
-//        return "SessionPlay/index";
-//    }
 }
