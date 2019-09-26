3bd72d14e454e7f593cb1d60673b806e21adde23
gfike19@gmail.com <Vaughn*573>
2019-09-26 16:25:39 -0500

    testied adding hashmaps to view and to session and then pulling them out of session, no issues

--- src/main/java/com/gfike/SessionPlay/MainController.java
+++ src/main/java/com/gfike/SessionPlay/MainController.java
@@ -1,6 +1,5 @@
 package com.gfike.SessionPlay;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,38 +7,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
 @Controller
-//@RequestMapping("SessionPlay")
 public class MainController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, HttpSession session)
     {
-        ArrayList<String> words = null;
-        if (session.getAttribute("words") == null) {
-            // where words is just a string
-            //String words = session.getAttribute("words").toString();
-            words = new ArrayList<String>();
+        HashMap<Integer, String> hmap;
+
+        if(session.getAttribute("hmap") != null){
+            hmap = (HashMap<Integer, String>) session.getAttribute("hmap");
         }
-        else {
-            words = (ArrayList<String>) session.getAttribute("words");
+        else{
+            hmap = new HashMap<>();
         }
-        session.setAttribute("words", words);
-        model.addAttribute("words", words);
+
+        model.addAttribute("hmap", hmap);
+        session.setAttribute("hmap", hmap);
+
         return "index";
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public String indexPost (HttpServletRequest request, Model model, HttpSession session) {
-        String sumText = request.getParameter("sumText");
-        ArrayList<String> words = (ArrayList<String>) session.getAttribute("words");
-        words.add(sumText);
-        // where words is just a string
-//        session.setAttribute("words", sumText);
-//
-        model.addAttribute("words", words);
+
+        Random rand = new Random();
+        int r = rand.nextInt(2147483647);
+
+        HashMap<Integer, String> hmap = (HashMap<Integer, String>) session.getAttribute("hmap");
+        String word = request.getParameter("sumText");
+        hmap.put(r, word);
+
+        model.addAttribute("hmap", hmap);
         return "redirect:";
     }
 
